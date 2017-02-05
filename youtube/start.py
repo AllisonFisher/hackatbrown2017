3d9d2cd8b61fcb10b016a6fdaadf90063de82ed2
Ethan Benowitz <ethanb1994@gmail.com>
2017-02-05 00:42:13 +0000

    Here's my number. Call me maybe.

--- youtube/start.py
+++ youtube/start.py
@@ -4,10 +4,19 @@ import sys
 import getopt
 import re
 import os
+import subprocess
+
+# make metadata file for frames
+def export_metadata(data):
+  print 'TODO'
+
+#def run_process(cmd):
+#  p = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+#  return iter(p.stdout.readline, b'')
 
 def main(argv):
   input_url = '' 
-  output_fps = 1
+  output_fps = '1/10'
 
   try:
     opts, args = getopt.getopt(argv, "hi:f:", ["input=", "fps="])
@@ -42,8 +51,15 @@ def main(argv):
   os.mkdir(video_id)
   os.system('youtube-dl {} -o \'{}/video.%(ext)s\''.format(input_url, video_id, video_id))
 
+  frame_data = {}
+
   os.mkdir('{}/frames'.format(video_id))
-  os.system('ffmpeg -i {}/video.mkv -vf fps={} -f image2 {}/frames/%03d.jpg'.format(video_id, output_fps, video_id))
+  cmd = 'ffmpeg -i {}/video.mp4 -vf fps={} -f image2 {}/frames/%03d.jpg'.format(video_id, output_fps, video_id)
+  os.system(cmd)
+
+  #for line in run_process(cmd):
+  #  print (line)
+
 
 if __name__ == "__main__":
   main(sys.argv[1:])
